7d16ecb1aea9c67d90640addc84abac821c82770
James Carr <jrcarr2@jrcarr2-laptop.(none)>
2010-02-05 12:47:42 -0600

    misnamed bean in spring contetx

--- api/src/main/java/org/jamescarr/Main.java
+++ api/src/main/java/org/jamescarr/Main.java
@@ -1,13 +1,13 @@
 package org.jamescarr;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
-import org.jamescarr.Foo;
+import org.jamescarr.Greeter;
 
 public class Main{
 	public static void main(String... args){
 		JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
 		factory.setAddress("http://localhost:8080/services/services/english");
 		factory.setServiceClass(Foo.class);
-		Foo client = (Foo)factory.create();
+		Greeter client = (Greeter)factory.create();
 
 		System.out.println(client.getGreetingFor("James"));
 	}
